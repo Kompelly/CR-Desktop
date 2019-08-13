4a782ddda6bb113670c10e9ad33c56d1cf8b7d13
sumankompelly <sumankompelly@10.0.126.233>
2019-08-13 13:36:32 -0400

    1

--- Test Suites/Live site.ts
+++ Test Suites/Live site.ts
@@ -12,7 +12,7 @@
             <profileName>default</profileName>
             <runConfigurationId>Chrome</runConfigurationId>
          </configuration>
-         <runEnabled>true</runEnabled>
+         <runEnabled>false</runEnabled>
          <testSuiteEntity>Test Suites/suite-chrome</testSuiteEntity>
       </TestSuiteRunConfiguration>
       <TestSuiteRunConfiguration>
